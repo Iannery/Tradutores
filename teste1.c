5cf6e5f6bef3cd3d0f81b42943687939a493dd96
Ian Nery <ian.muxis@gmail.com>
2021-02-15 19:34:21 -0300

    primeiro commit

---
+++ teste1.c
@@ -0,0 +1,14 @@
+int main(){
+    int a; 
+    int %b; float c;
+    a = 5;
+
+    if(a == 5){
+        writeln("alo");
+    }
+    else{
+        b += 5;
+    }
+    c = 818239aikjsdoaklsd;
+    return 0;
+}
---
+++ teste2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int akmsidsijndjfsmlkdanmskjdandoailkjsmdfsdgmskjldfjaolskidjmadasdmfsdklijfajsmdolskmfolsmdsjkldmasdeakijmsdea;
+
+    akmsidsijndjfsmlkdanmskjdandoailkjsmdfsdgmskjldfjaolskidjmadasdmfsdklijfajsmdolskmfolsmdsjkldmasdeakijmsdea = 0;
+
+    printf("%d\n", akmsidsijndjfsmlkdanmskjdandoailkjsmdfsdgmskjldfjaolskidjmadasdmfsdklijfajsmdolskmfolsmdsjkldmasdeakijmsdea);
+    return 0;
+}
